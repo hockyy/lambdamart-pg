beb91cc74b1f2c3d69737db7b9e1a16b91ffcbed
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-11-15 00:26:44 +0700

    Add ipynb and letor model

--- letor.py
+++ letor.py
@@ -15,7 +15,7 @@ from scipy.spatial.distance import cosine
 import lightgbm
 
 def set_up_data():
-    cprint("Step 1: Setting up data","yellow")
+    cprint("Setting up data","yellow")
     cprint("Downloading corpus...","blue")
     proc = subprocess.Popen("wget -c https://www.cl.uni-heidelberg.de/statnlpgroup/nfcorpus/nfcorpus.tar.gz -P data".split())
     proc.wait()
@@ -79,7 +79,6 @@ class Model(CorpusDocuments, CorpusQueries):
         self.q_docs_rel = dict()
         self.init_qrels(qrel_path)
         self.generate_dataset()
-        self.generate_lsa()
         cprint(f"Generated model", "yellow")
         pass
     
@@ -109,6 +108,9 @@ class Model(CorpusDocuments, CorpusQueries):
         # test
         # print("number of Q-D pairs:", len(self.dataset))
         # print("self.group_qid_count:", self.group_qid_count)
+        self.dictionary = Dictionary()
+        self.bow_corpus = [self.dictionary.doc2bow(doc, allow_update=True) for doc in self.documents.values()]
+
         assert sum(self.group_qid_count) == len(self.dataset), "ada yang salah"
         cprint(f"Generated Dataset", "green")
         # print(self.dataset[:2])
@@ -116,16 +118,28 @@ class Model(CorpusDocuments, CorpusQueries):
     def generate_lsa(self):
 
         cprint(f"Generating LSA", "blue")
-        
-        self.dictionary = Dictionary()
-        self.bow_corpus = [self.dictionary.doc2bow(doc, allow_update = True) for doc in self.documents.values()]
-        self.LsiModel = LsiModel(self.bow_corpus, num_topics = self.NUM_LATENT_TOPICS) # 200 latent topics
-
+        self.lsi_model = LsiModel(self.bow_corpus, num_topics = self.NUM_LATENT_TOPICS) # 200 latent topics
         cprint(f"Generated LSA", "green")
+        self.save_lsa()
         # test melihat representasi vector dari sebuah dokumen & query
-    
+
+    def save_lsa(self):
+        current_filename = f'lsa-{int(datetime.datetime.now().timestamp())}.pkl'
+        cprint(f"Saving lsa to {current_filename}", "blue")
+        with open(current_filename, 'wb') as f:
+            pickle.dump([self.lsi_model], f)
+        cprint(f"Saved", "green")
+
+    def load_lsa(self, timestamp):
+        current_filename = f'lsa-{timestamp}.pkl'
+        cprint(f"Loading lsa {current_filename}", "blue")
+        with open(current_filename, 'rb') as f:
+            [self.lsi_model] = pickle.load(f)
+        cprint(f"Loaded", "green")
+
     def vector_rep(self, text):
-        rep = [topic_value for (_, topic_value) in self.LsiModel[self.dictionary.doc2bow(text)]]
+        assert self.lsi_model != None, "Run generate or lsa model first"
+        rep = [topic_value for (_, topic_value) in self.lsi_model[self.dictionary.doc2bow(text)]]
         return rep if len(rep) == self.NUM_LATENT_TOPICS else [0.] * self.NUM_LATENT_TOPICS
 
     def get_features(self, query, doc):
@@ -178,19 +192,21 @@ class Model(CorpusDocuments, CorpusQueries):
 
     def save_model(self):
         current_filename = f'model-{int(datetime.datetime.now().timestamp())}.pkl'
-        cprint(f"Saving model to {current_filename}")
+        cprint(f"Saving model to {current_filename}", "blue")
         with open(current_filename, 'wb') as f:
             pickle.dump([self.ranker], f)
-        cprint(f"Saved")
+        cprint(f"Saved", "green")
 
     def load_model(self, timestamp):
         current_filename=f'model-{timestamp}.pkl'
-        cprint(f"Loading model {current_filename}")
+        cprint(f"Loading model {current_filename}", "blue")
         with open(current_filename, 'rb') as f:
             [self.ranker] = pickle.load(f)
-        cprint(f"Loaded")
+        cprint(f"Loaded", "green")
 
     def predict(self, query, docs):
+        assert self.lsi_model != None, "Run generate_lsa or load_lsa first"
+        assert self.ranker != None, "Run train_model or load_model first"
         X_unseen = []
         for doc_id, doc in tqdm(docs):
             X_unseen.append(self.get_features(query.split(), doc.split()))
@@ -222,8 +238,10 @@ def main():
     # print(model)
     # print(model.vector_rep(model.documents["MED-329"]))
     # print(model.vector_rep(model.queries["PLAIN-2435"]))
+    # model.generate_lsa()
+    model.load_lsa(1668446602)
     # model.train_model()
-    model.load_model(1668445548)
+    model.load_model(1668446623)
     query = "how much cancer risk can be avoided through lifestyle change ?"
 
     docs =[("D1", "dietary restriction reduces insulin-like growth factor levels modulates apoptosis cell proliferation tumor progression num defici pubmed ncbi abstract diet contributes one-third cancer deaths western world factors diet influence cancer elucidated reduction caloric intake dramatically slows cancer progression rodents major contribution dietary effects cancer insulin-like growth factor igf-i lowered dietary restriction dr humans rats igf-i modulates cell proliferation apoptosis tumorigenesis mechanisms protective effects dr depend reduction multifaceted growth factor test hypothesis igf-i restored dr ascertain lowering igf-i central slowing bladder cancer progression dr heterozygous num deficient mice received bladder carcinogen p-cresidine induce preneoplasia confirmation bladder urothelial preneoplasia mice divided groups ad libitum num dr num dr igf-i igf-i/dr serum igf-i lowered num dr completely restored igf-i/dr-treated mice recombinant igf-i administered osmotic minipumps tumor progression decreased dr restoration igf-i serum levels dr-treated mice increased stage cancers igf-i modulated tumor progression independent body weight rates apoptosis preneoplastic lesions num times higher dr-treated mice compared igf/dr ad libitum-treated mice administration igf-i dr-treated mice stimulated cell proliferation num fold hyperplastic foci conclusion dr lowered igf-i levels favoring apoptosis cell proliferation ultimately slowing tumor progression mechanistic study demonstrating igf-i supplementation abrogates protective effect dr neoplastic progression"), 
