8e77656f4fac1d756628f4f62681c9aa7febc6ba
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-11-14 21:52:53 +0700

    Add letor

--- letor.py
+++ letor.py
@@ -1,8 +1,18 @@
-import os
+import subprocess
+from termcolor import colored, cprint
 
 def set_up_data():
-    os.system("wget -c https://www.cl.uni-heidelberg.de/statnlpgroup/nfcorpus/nfcorpus.tar.gz -P data")
-    os.system("tar -xvf data/nfcorpus.tar.gz")  
+    cprint("Downloading corpus...","blue")
+    proc = subprocess.Popen("wget -c https://www.cl.uni-heidelberg.de/statnlpgroup/nfcorpus/nfcorpus.tar.gz -P data".split())
+    proc.wait()
+    cprint("Extracting corpus...","blue")
+    proc = subprocess.Popen("tar -xvf data/nfcorpus.tar.gz".split())
+    proc.wait()
+    # out, err = proc.communicate()
+    # print(out, err)
+    # os.system()
 
 def main():
     set_up_data()
+
+main()
